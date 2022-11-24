2dfd8a35b555757ff0f0eb88a39df5f89c665521
mingendo <amazing-hash@yandex.ru>
2022-11-24 19:16:42 +0400

    feat: Паттерн Хранитель

---
+++ src/bin/memento.rs
@@ -0,0 +1,64 @@
+/*
+* Шаблон «Хранитель» фиксирует и хранит текущее состояние объекта, чтобы оно легко восстанавливалось.
+* Шаблон «Хранитель» позволяет восстанавливать объект в его предыдущем состоянии (отмена через откат — undo via rollback).
+ */
+struct MementoArticle {
+    state: String
+}
+
+impl MementoArticle {
+    fn new(state: &str) -> Self {
+        MementoArticle { state: state.to_string() }
+    }
+    fn get_state(&mut self) -> &str {
+        &self.state
+    }
+}
+
+struct Article {
+    content: Option<String>,
+    mementos: Vec<MementoArticle>
+}
+
+impl Article {
+    fn new() -> Self {
+        Article { content: None, mementos: vec![]}
+    }
+    fn undo(&mut self) {
+        match self.mementos.pop() {
+            Some(mut memento) => self.content = Some(memento.get_state().to_string()),
+            None => self.content = None
+        }
+    }
+    fn get_content(&self) -> Option<&str> {
+        if let Some(ref content) = self.content {
+            return Some(content)
+        }
+        None
+    }
+    fn set_content(&mut self, content: &str) {
+        if let Some(ref content) = self.content {
+            self.mementos.push(MementoArticle::new(content));
+        }
+        self.content = Some(content.to_string());
+    }
+}
+
+fn main(){
+    let mut article = Article::new();
+    println!("{:?}", article.get_content());
+    article.set_content("First content");
+    println!("{:?}", article.get_content());
+    article.set_content("Second content");
+    println!("{:?}", article.get_content());
+    article.undo();
+    println!("{:?}", article.get_content());
+    article.undo();
+    println!("{:?}", article.get_content());
+    article.undo();
+    println!("{:?}", article.get_content());
+    article.set_content("Third content");
+    println!("{:?}", article.get_content());
+    article.undo();
+    println!("{:?}", article.get_content());
+}
