bc588c06b96660f8158dbfe575b682acaff6eaaa
mingendo <amazing-hash@yandex.ru>
2022-12-06 21:27:38 +0400

    feat: factory

---
+++ src/bin/abstract_factory.rs
@@ -0,0 +1,50 @@
+/**
+ * Предоставляет интерфейс для создания семейств взаимосвязанных или 
+ * взаимозависимых объектов, не специфийируя их конкретныъ классов
+ *
+ * Применимость:
+ * - когда система не должна зависеть от того, как создаюся, компонуются и 
+ *   представляются входищие в нее объекты
+ * - когда входящие в семейство взаимосвязанные объекты должны использоваться 
+ *   вместе и вам необходимо обеспечить выполнение этого ограничения
+ * - когда система должна конфигурировать одним из семейств составляющих ее
+ *   объектов
+ * - когда вы хотите предоставить библиотеку объектов, раскрывая только их 
+ *   интерфейсы, но не реализацию
+**/
+
+trait Button {
+    fn click(&self);
+}
+struct ButtonQT {}
+impl Button for ButtonQT {
+    fn click(&self) {
+        println!("Clicked QT button");
+    }
+}
+struct ButtonGTK {}
+impl Button for ButtonGTK {
+    fn click(&self) {
+        println!("Clicked GTK button");
+    }
+}
+trait WidgetFactory {
+    fn button(&self) -> Box<dyn Button>;
+}
+struct QtWidgetFactory {}
+impl WidgetFactory for QtWidgetFactory {
+    fn button(&self) -> Box<dyn Button> {
+        Box::new(ButtonQT {})
+    }
+}
+struct GtkWidgetFactory {}
+impl WidgetFactory for GtkWidgetFactory {
+    fn button(&self) -> Box<dyn Button> {
+        Box::new(ButtonGTK {})
+    }
+}
+fn main() {
+    let factory = GtkWidgetFactory {};
+    let button = factory.button();
+    button.click();
+}
--- src/bin/factory.rs
+++ src/bin/factory.rs
@@ -1,50 +1,57 @@
 /**
- * Предоставляет интерфейс для создания семейств взаимосвязанных или 
- * взаимозависимых объектов, не специфийируя их конкретныъ классов
+ * В объектно ориентированном программировании фабрикой называется объект,
+ * создающий другие объекты. Формально фабрика — это функция или метод,
+ * возвращающая объекты разных прототипов или классов из вызова какого-то
+ * метода, который считается новым.
  *
- * Применимость:
- * - когда система не должна зависеть от того, как создаюся, компонуются и 
- *   представляются входищие в нее объекты
- * - когда входящие в семейство взаимосвязанные объекты должны использоваться 
- *   вместе и вам необходимо обеспечить выполнение этого ограничения
- * - когда система должна конфигурировать одним из семейств составляющих ее
- *   объектов
- * - когда вы хотите предоставить библиотеку объектов, раскрывая только их 
- *   интерфейсы, но не реализацию
+ * Простая фабрика просто генерирует экземпляр для клиента без предоставления
+ * какой-либо логики экземпляра.
+ *
+ * Когда использовать:
+ * - когда создание объекта подразумевает какую-то логику, а не просто
+ *   несколько присваиваний, то имеет смысл делегировать задачу выделенной
+ *   фабрике, а не повторять повсюду один и тот же код.
 **/
 
-trait Button {
-    fn click(&self);
+#[derive(Debug)]
+#[allow(unused)]
+enum Color {
+    White,
+    Black,
 }
-struct ButtonQT {}
-impl Button for ButtonQT {
-    fn click(&self) {
-        println!("Clicked QT button");
+
+#[derive(Debug)]
+#[allow(unused)]
+struct Door {
+    name: String,
+    color: Color,
+}
+
+impl Door {
+    fn new(name: &str, color: Color) -> Self {
+        Door {
+            name: name.to_string(),
+            color,
+        }
     }
 }
-struct ButtonGTK {}
-impl Button for ButtonGTK {
-    fn click(&self) {
-        println!("Clicked GTK button");
+
+struct DoorFactory {}
+
+impl DoorFactory {
+    fn make(name: &str, color: Color) -> Option<Door> {
+        /*
+         * Какая та дополнительная логика, которую не стоит раскрывать или
+         * дополнительные проверки
+         */
+        if name.len() > 10 {
+            return None;
+        }
+        Some(Door::new(name, color))
     }
 }
-trait WidgetFactory {
-    fn button(&self) -> Box<dyn Button>;
-}
-struct QtWidgetFactory {}
-impl WidgetFactory for QtWidgetFactory {
-    fn button(&self) -> Box<dyn Button> {
-        Box::new(ButtonQT {})
-    }
-}
-struct GtkWidgetFactory {}
-impl WidgetFactory for GtkWidgetFactory {
-    fn button(&self) -> Box<dyn Button> {
-        Box::new(ButtonGTK {})
-    }
-}
+
 fn main() {
-    let factory = GtkWidgetFactory {};
-    let button = factory.button();
-    button.click();
+    let door = DoorFactory::make("Wood Door", Color::White).unwrap();
+    println!("{:?}", door);
 }
