f11d8f7b166f51d0770668ed6e26ebe88349a54e
mingendo <amazing-hash@yandex.ru>
2022-12-08 13:38:06 +0400

    refactor: memento

--- src/bin/memento.rs
+++ src/bin/memento.rs
@@ -1,53 +1,67 @@
 /**
-* Не нарушая инкапсуляции, фиксирует и выносит за пределы объекта его внутреннее
-* состояние так, чтобы позднее можно было восстановить в нем объект.
+* Шаблон «Хранитель» фиксирует и хранит текущее состояние объекта, чтобы оно 
+* легко восстанавливалось.
 *
-* Применимость:
-* - необходимо сохранить мгновенный снимок состояния объекта (или его части),
-*   чтобы впоследствии объект можно было восстановить в том же состоянии
-* - прямое получение этого состояния раскрывает детали реализации и нарушает
+* Шаблон «Хранитель» позволяет восстанавливать объект в его предыдущем состоянии
+* (отмена через откат — undo via rollback).
+*
+* Когда использовать:
+* - когда необходимо сохранить мгновенный снимок состояния объекта
+*   (или его части), чтобы впоследствии объект можно было восстановить в том же 
+*   состоянии
+* - когда прямое получение этого состояния раскрывает детали реализации и нарушает
 *   инкапсуляцию объекта
 **/
 
 struct MementoArticle {
-    state: String,
+    content: String,
+}
+impl MementoArticle {
+    fn new(content: &str) -> Self {
+        MementoArticle { content: content.to_string() }
+    }
+    fn get_content(self) -> String {
+        self.content
+    }
 }
 
 struct Article {
     content: String,
+    memento: Option<MementoArticle>
 }
 
 impl Article {
     fn new() -> Self {
         Article {
             content: Default::default(),
-        }
-    }
-    fn set_memento(&mut self, memento: MementoArticle) {
-        self.content = memento.state;
-    }
-    fn get_memento(&self) -> MementoArticle {
-        MementoArticle {
-            state: self.content.clone(),
+            memento: Default::default()
         }
     }
     fn set_content(&mut self, content: &str) {
         self.content = content.to_string();
     }
-    fn get_content(&self) -> &str {
+    fn get_content(&mut self) -> &str {
         &self.content
     }
+    fn save(&mut self) {
+        self.memento = Some(MementoArticle::new(&self.content));
+    }   
+    fn restore(&mut self){
+        if let Some(memento) = self.memento.take() {
+            self.content = memento.get_content();
+        }
+    }
+    
 }
 
 fn main() {
     let mut article = Article::new();
     article.set_content("First content");
     println!("{:?}", article.get_content());
-
-    let memento = article.get_memento();
+    article.save();
 
     article.set_content("Second content");
     println!("{:?}", article.get_content());
-    article.set_memento(memento);
+    article.restore();
     println!("{:?}", article.get_content());
 }
