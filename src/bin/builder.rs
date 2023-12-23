c07560f475bad5ed68ffea993d7975bec3bbcc26
amazing-hash <amazing-hash@yandex.ru>
2023-12-23 14:05:38 +0400

    refactor: builder

--- src/bin/builder.rs
+++ src/bin/builder.rs
@@ -1,62 +1,85 @@
 /**
  * Шаблон «Строитель» предназначен для поиска решения проблемы антипаттерна
  * Telescoping constructor.
- * 
- * Шаблон позволяет создавать разные свойства объекта, избегая загрязнения 
+ *
+ * Шаблон позволяет создавать разные свойства объекта, избегая загрязнения
  * конструктора (constructor pollution). Это полезно, когда у объекта может быть
  * несколько свойств. Или когда создание объекта состоит из большого количества
  * этапов.  
- * 
+ *
  * Когда использовать:
- * - когда у объекта может быть несколько свойств и когда нужно избежать 
- *   Telescoping constructor. Ключевое отличие от шаблона «Простая фабрика»: 
+ * - когда у объекта может быть несколько свойств и когда нужно избежать
+ *   Telescoping constructor. Ключевое отличие от шаблона «Простая фабрика»:
  *   он используется в одноэтапном создании, а «Строитель» — в многоэтапном.
  **/
-#[derive(Debug)]
-enum Color {
-    Black,
-    White,
+#[allow(unused)]
+enum TransmissionKind {
+    Auto,
+    Manual,
 }
-
-#[derive(Debug)]
+#[allow(unused)]
+enum EngineKind {
+    Gasoline,
+    Diesel,
+}
+#[allow(unused)]
+enum NavigationKind {
+    Gps,
+    Glonas,
+}
 #[allow(unused)]
 struct Car {
-    name: String,
-    color: Color,
+    transmission: TransmissionKind,
+    engine: EngineKind,
+    navigation: Option<NavigationKind>,
 }
 
 struct CarBuilder {
-    name: String,
-    color: Color,
+    transmission: Option<TransmissionKind>,
+    engine: Option<EngineKind>,
+    navigation: Option<NavigationKind>,
 }
 
 impl CarBuilder {
     fn new() -> Self {
         CarBuilder {
-            name: "No name".to_string(),
-            color: Color::White,
+            transmission: None,
+            engine: None,
+            navigation: None,
         }
     }
-    fn build(self) -> Car {
-        Car {
-            name: self.name,
-            color: self.color,
+    fn build(mut self) -> Option<Car> {
+        // Проверка построения объекта, только навигатор опционален
+        if self.engine.is_some() && self.transmission.is_some() {
+            return Some(Car {
+                transmission: self.transmission.unwrap(),
+                engine: self.engine.unwrap(),
+                navigation: self.navigation.take(),
+            });
         }
+        None
     }
-    fn name(mut self, name: &str) -> Self {
-        self.name = name.to_string();
+    fn transmission(mut self, transmission: TransmissionKind) -> Self {
+        self.transmission.replace(transmission);
         self
     }
-    fn paint(mut self, color: Color) -> Self {
-        self.color = color;
+    fn engine(mut self, engine: EngineKind) -> Self {
+        self.engine.replace(engine);
+        self
+    }
+    fn navigation(mut self, navigation: NavigationKind) -> Self {
+        self.navigation.replace(navigation);
         self
     }
 }
 
 fn main() {
-    let car = CarBuilder::new()
-        .name("Lada Vesta")
-        .paint(Color::Black)
-        .build();
-    println!("{:?}", car);
+    if let Some(_car) = CarBuilder::new()
+        .engine(EngineKind::Gasoline)
+        .transmission(TransmissionKind::Auto)
+        .navigation(NavigationKind::Glonas)
+        .build()
+    {
+        println!("The car was built successfully")
+    }
 }
